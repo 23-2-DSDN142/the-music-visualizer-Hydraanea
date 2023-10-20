92ab5e8a9b34040e6957472644ca08d5eb856d0a
Hydraanea <132314937+Hydraanea@users.noreply.github.com>
2023-10-20 16:12:12 +1300

    just some final checks.

--- music_starter.js
+++ music_starter.js
@@ -120,12 +120,12 @@ pop();
  
 
 
+// LYRICS
 
- // Bottom Lyrics
-  textFont('Georgia'); // please use CSS safe fonts
+  textFont('Georgia'); 
+  // textFont('Helvetica');
   textSize(20);
 
-   // display "words"
    noStroke();
    textAlign(CENTER);
 
@@ -141,7 +141,8 @@ pop();
 
 
 
-// white dots
+// MOVING DOTS
+
 noStroke()
 fill(purpleColor);
 ellipse(300, drumHeight, ballSize);
@@ -163,7 +164,7 @@ ellipse(1000, drumHeight, ballSize);
 
 
 
-
+// SOUND BAR --> DRUM
 
    strokeWeight(3);
    stroke(drum, 10, 10);
@@ -187,6 +188,8 @@ ellipse(1000, drumHeight, ballSize);
      
    }
    
+
+// SOUND BAR --> BASS
   
    strokeWeight(3);
    stroke(bass, 10, 10);
@@ -208,6 +211,8 @@ ellipse(1000, drumHeight, ballSize);
 
    }
 
+
+   // SOUND BAR --> OTHER
      
    strokeWeight(3);
    stroke(other, 10, 10);
@@ -228,6 +233,9 @@ ellipse(1000, drumHeight, ballSize);
      line(lineStart, lineStep+20, lineEnd, lineStep+20);
 
    }
+
+
+// SOUND BAR --> VOCAL
 
    strokeWeight(3);
    stroke(vocal, 10, 10);
@@ -254,35 +262,35 @@ ellipse(1000, drumHeight, ballSize);
 
 // Orginal Code
 
-  //  let bar_spacing = height / 10;
-  //  let bar_height = width / 12;
-  //  let bar_pos_x = width / 2;
+//    let bar_spacing = height / 10;
+//    let bar_height = width / 12;
+//    let bar_pos_x = width / 2;
 
 
-  //  // vocal bar is red
-  //  fill(200, 0, 0);
-  //  rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-  //  fill(0);
-  //  text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
+//    // vocal bar is red
+//    fill(200, 0, 0);
+//    rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
+//    fill(0);
+//    text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
  
-  //  // drum bar is green
-  //  fill(0, 200, 0);
-  //  rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-  //  fill(0);
-  //  text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
+//    // drum bar is green
+//    fill(0, 200, 0);
+//    rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
+//    fill(0);
+//    text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
  
-  //  // bass bar is blue
-  //  fill(50, 50, 240);
-  //  rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-  //  fill(0);
-  //  text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
+//    // bass bar is blue
+//    fill(50, 50, 240);
+//    rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
+//    fill(0);
+//    text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
  
-  //  // other bar is white
-  //  fill(200, 200, 200);
-  //  rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-  //  fill(0);
-  //  text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-  //  fill(255, 255, 0);
+//    // other bar is white
+//    fill(200, 200, 200);
+//    rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
+//    fill(0);
+//    text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
+//    fill(255, 255, 0);
 
  
 }
